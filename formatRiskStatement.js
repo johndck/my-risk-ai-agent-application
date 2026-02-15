8a0bb35ced78ecb5530b23afade3902fba9310ea
John Dick <johnturnerdick@gmail.com>
2026-02-15 20:17:06 +0000

    added this formatting function

---
+++ formatRiskStatement.js
@@ -0,0 +1,8 @@
+
+
+function formatRiskStatement(data) {
+    const { if_Cause, then_Event, resultingIn_Consequence } = data.riskComponents;
+    return `There is a risk that if ${if_Cause}, then ${then_Event}, resulting in ${resultingIn_Consequence}.`;
+  }
+
+  export default formatRiskStatement

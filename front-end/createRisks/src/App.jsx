c9207661be5f2d79b9cf67ed0a1dc512dc80b3df
John Dick <johnturnerdick@gmail.com>
2026-03-02 21:08:14 +0000

    basic prop reminder

--- front-end/createRisks/src/App.jsx
+++ front-end/createRisks/src/App.jsx
@@ -4,15 +4,21 @@ import './App.css'
 import Post from './components/test'
 
 function App() {
+
+
+  // Create the function in the parent component.
+  const onClick = () => {
+    console.log('Button clicked');
+  }
   
 
   return (
     <>
       <p>Hello World</p>
-      <Post postdata={{ username: 'John', content: 'Hello World', likes: 10, isVerified: true }} />
-      <Post postdata={{ username: 'Jane', content: 'Hello World', likes: 10, isVerified: true }} />
-      <Post postdata={{ username: 'Jim', content: 'Hello World', likes: 10, isVerified: true }} />
-      <Post postdata={{ username: 'Jill', content: 'Hello World', likes: 10, isVerified: true }} />
+      <Post onClick={onClick} postdata={{ username: 'John', content: 'Hello World', likes: 10, isVerified: true}}  />
+      <Post postdata={{ username: 'Jane', content: 'Hello World', likes: 10, isVerified: true}}  />
+      <Post postdata={{ username: 'Jim', content: 'Hello World', likes: 10, isVerified: true }}  />
+      <Post postdata={{ username: 'Jill', content: 'Hello World', likes: 10, isVerified: true }}  />
       <Post postdata={{ username: 'Jack', content: 'Hello World', likes: 10, isVerified: true }} />
       <Post postdata={{ username: 'Jill', content: 'Hello World', likes: 10, isVerified: true }} />
     </>
--- front-end/createRisks/src/components/test.jsx
+++ front-end/createRisks/src/components/test.jsx
@@ -1,9 +1,10 @@
-function Post({ postdata: { username, content, likes, isVerified } }) {
+function Post({ onClick, postdata: { username, content, likes, isVerified } }) {
     return (
       <div>
         <p>{`${username} - ${content}`}</p>
         <p>{`Likes: ${likes}`}</p>
         <p>{`Verified: ${isVerified ? 'Yes' : 'No'}`}</p>
+        <button type="button" onClick={onClick}>Click Me!</button>
       </div>
     );
   }

1c3545c2657cb8573dd6712e0d916380a9037db5
John Dick <johnturnerdick@gmail.com>
2026-03-08 12:29:52 +0000

    expanded app scope

--- front-end/createRisks/src/App.jsx
+++ front-end/createRisks/src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import './index.css'
+import Section from './components/section.jsx'
 import MainWrapper from './components/test.jsx'
 
 function App() {
--- front-end/createRisks/src/components/test.jsx
+++ front-end/createRisks/src/components/test.jsx
@@ -1,11 +1,15 @@
-
+import Section from './section.jsx'
 
 
 
 function MainWrapper (){
     return (
-        <div className="h-screen overflow-y-auto snap-y snap-mandatory border border-red-500" >
-            <h1>Core components will go here</h1>
+        <div className="h-screen overflow-y-auto snap-y snap-mandatory no-scrollbar border border-red-500" >
+           
+
+            <Section title="Section landing" bgColor="bg-red-500" />
+            <Section title="Section 2" bgColor="bg-blue-500" />
+            <Section title="Section 3" bgColor="bg-green-500" />
         </div>
     )
 }
